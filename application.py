b7fa110e8019e56c67e88c464a9a6f4043bc331d
six2dez <alexis6307@gmail.com>
2022-12-02 10:52:44 +0100

    Add files via upload

---
+++ application.py
@@ -0,0 +1,49 @@
+import logging.handlers
+
+# Create logger
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+# Handler
+LOG_FILE = '/tmp/sample-app.log'
+handler = logging.handlers.RotatingFileHandler(LOG_FILE, maxBytes=1048576, backupCount=5)
+handler.setLevel(logging.INFO)
+
+# Formatter
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+# Add Formatter to Handler
+handler.setFormatter(formatter)
+
+# add Handler to Logger
+logger.addHandler(handler)
+
+welcome = """
+sto poc by six2dez
+"""
+
+def application(environ, start_response):
+    path = environ['PATH_INFO']
+    method = environ['REQUEST_METHOD']
+    if method == 'POST':
+        try:
+            if path == '/':
+                request_body_size = int(environ['CONTENT_LENGTH'])
+                request_body = environ['wsgi.input'].read(request_body_size)
+                logger.info("Received message: %s" % request_body)
+            elif path == '/scheduled':
+                logger.info("Received task %s scheduled at %s", environ['HTTP_X_AWS_SQSD_TASKNAME'],
+                            environ['HTTP_X_AWS_SQSD_SCHEDULED_AT'])
+        except (TypeError, ValueError):
+            logger.warning('Error retrieving request body for async work.')
+        response = ''
+    else:
+        if path == '/tkopocsix2dez':
+            response = welcome
+        else:
+            response = 'STO by six2dez'
+        start_response("200 OK", [
+        ("Content-Type", "text/html"),
+        ("Content-Length", str(len(response)))
+        ])
+    return [bytes(response, 'utf-8')]
